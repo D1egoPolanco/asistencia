81230c7f833702ff7beac7ad66671fa3f1e5d868
d1egopolanco <00301417@uca.edu.sv>
2019-05-22 13:55:37 -0600

    falta tags

--- app/src/main/java/com/example/taller_4_pdm/Repository/BookRepository.kt
+++ app/src/main/java/com/example/taller_4_pdm/Repository/BookRepository.kt
@@ -1,16 +1,45 @@
 package com.example.taller_4_pdm.Repository
 
-import androidx.lifecycle.LiveData
-import com.example.taller_4_pdm.Models.Book
+import androidx.annotation.WorkerThread
+//import androidx.lifecycle.LiveData
+//import com.example.taller_4_pdm.Models.Author
+//import com.example.taller_4_pdm.Models.Book
 import com.example.taller_4_pdm.RoomDataBase.Daos.AuthorDao
 import com.example.taller_4_pdm.RoomDataBase.Daos.BookDao
 import com.example.taller_4_pdm.RoomDataBase.Daos.EditorialDao
+import com.example.taller_4_pdm.RoomDataBase.Entities.AuthorEntity
+import com.example.taller_4_pdm.RoomDataBase.Entities.BookEntity
+import com.example.taller_4_pdm.RoomDataBase.Entities.EditorialEntity
+import com.example.taller_4_pdm.RoomDataBase.Entities.TagEntity
 
 
-class BookRepository  (private val bookDao:BookDao, private val autorDao:AuthorDao, private val editorialDao:EditorialDao, private val tagsDao: EditorialDao){
+class BookRepository  (private val bookDao:BookDao, private val authorDao: AuthorDao,
+                       private val editorialDao:EditorialDao, private val tagsDao: EditorialDao){
+
+    @WorkerThread
+    suspend fun insertBook(bookEntity: BookEntity){
+        bookDao.insert(bookEntity)
+    }
+
+
+    @WorkerThread
+    suspend fun insertAutor(authorEntity: AuthorEntity){
+        authorDao.insert(authorEntity)
+    }
+
+    @WorkerThread
+    suspend fun insertEditorial(editorialEntity: EditorialEntity){
+        editorialDao.insert(editorialEntity)
+    }
+
+  /* @WorkerThread
+   suspend fun insertTag (tagEntity: TagEntity){
+       editorialDao.insert(tagEntity)
+   }
+*/
+    }
 
 
 
 
 
-}
