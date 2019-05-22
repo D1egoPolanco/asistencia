e3ed6c4487b527953d48afcb2a16254229943631
d1egopolanco <00301417@uca.edu.sv>
2019-05-22 13:08:28 -0600

    clase repositorio

---
+++ app/src/main/java/com/example/taller_4_pdm/Repository/BookRepository.kt
@@ -0,0 +1,12 @@
+package com.example.taller_4_pdm.Repository
+
+import androidx.lifecycle.LiveData
+import com.example.taller_4_pdm.Models.Book
+import com.example.taller_4_pdm.RoomDataBase.BookDao
+
+class BookRepository  (private val bookDao:BookDao){
+
+
+
+
+}
