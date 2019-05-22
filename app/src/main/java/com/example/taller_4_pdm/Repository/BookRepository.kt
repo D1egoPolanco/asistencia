8c8e84b72ef84a5eb3584375ccacdd02194e6622
d1egopolanco <00301417@uca.edu.sv>
2019-05-22 13:17:18 -0600

    clase repositorio

--- app/src/main/java/com/example/taller_4_pdm/Repository/BookRepository.kt
+++ app/src/main/java/com/example/taller_4_pdm/Repository/BookRepository.kt
@@ -2,9 +2,13 @@ package com.example.taller_4_pdm.Repository
 
 import androidx.lifecycle.LiveData
 import com.example.taller_4_pdm.Models.Book
-import com.example.taller_4_pdm.RoomDataBase.BookDao
+import com.example.taller_4_pdm.RoomDataBase.Daos.AuthorDao
+import com.example.taller_4_pdm.RoomDataBase.Daos.BookDao
+import com.example.taller_4_pdm.RoomDataBase.Daos.EditorialDao
 
-class BookRepository  (private val bookDao:BookDao){
+
+class BookRepository  (private val bookDao:BookDao, private val autorDao:AuthorDao, private val editorialDao:EditorialDao, private val tagsDao: EditorialDao){
+
 
 
 
