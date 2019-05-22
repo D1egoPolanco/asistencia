ef4443ba39a67be1659505cd6fbe62672df8a0bd
Fer <00017917@uca.edu.sv>
2019-05-22 13:01:41 -0600

    agregadas 2 entities y sus daos. Haciendo foreign keys ahorita

--- app/src/main/java/com/example/taller_4_pdm/RoomDataBase/Entities/AuthorEntity.kt
+++ app/src/main/java/com/example/taller_4_pdm/RoomDataBase/Entities/AuthorEntity.kt
@@ -5,20 +5,21 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.example.taller_4_pdm.Models.Book
+
 @Entity(tableName = "AuthorTable")
 data class AuthorEntity (
-    @PrimaryKey(autoGenerate = true) val id_author : Long,
-
     @ColumnInfo(name = "a_name")
     val name:String,
     @ColumnInfo(name = "a_books_written")
-    val books_written: ArrayList<Book>
-)
-/*
-@Entity
-    (foreignKeys = arrayOf(
+    val books_written: List<Book>
+){
+    @PrimaryKey(autoGenerate = true)
+    val id_author : Long = 0
+}/*
+@Entity(foreignKeys = arrayOf(
     ForeignKey(entity = AuthorEntity::class,
     parentColumns = arrayOf("id_author"),
-    childColumns = arrayOf("id_author_book")
-)))*/
+    childColumns = arrayOf("id_author_book"),
+    onDelete = ForeignKey.CASCADE)
+))*/
 
