1bee8d1fa88fba6f21a2034184d7add7957f3c1b
Luisa Arevalo <00038617@uca.edu.sv>
2019-05-22 13:01:58 -0600

    Fixing Book

--- app/src/main/java/com/example/taller_4_pdm/Models/Book.kt
+++ app/src/main/java/com/example/taller_4_pdm/Models/Book.kt
@@ -1,55 +1,13 @@
 package com.example.taller_4_pdm.Models
 
-import android.os.Parcel
-import android.os.Parcelable
-//Falta Modificar
 data class Book(
     val Caratula: String = "N/A",
     val Titulo: String = "N/A",
-    val Autores: ArrayList<Author> = arrayListOf() ,
+    val Autores: List<Author> = emptyList() ,
     val Edicion: Int = 0,
     val Editorial: String = "N/A",
     val ISBN: String = "N/A",
     val Resumen: String = "N/A",
-    val Tag: ArrayList<String> = arrayListOf(),
+    val Tag: List<String> = emptyList(),
     val Favorito: Boolean = false
-): Parcelable {
-    constructor(parcel : Parcel):this(
-            Caratula = parcel.readString(),
-            Titulo = parcel.readString(),
-            Autores = parcel.readArrayList(Book.javaClass.classLoader) as ArrayList<Author>,
-            Edicion = parcel.readInt(),
-            Editorial = parcel.readString(),
-            ISBN = parcel.readString(),
-            Resumen = parcel.readString(),
-            Tag = parcel.readArrayList(Book.javaClass.classLoader) as ArrayList<String>,
-            Favorito = parcel.readByte() != 0.toByte()
-    )
-    //Falta modificar
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(Caratula)
-        parcel.writeString(Titulo)
-        parcel.writeArray(arrayOf(Autores))
-        parcel.writeInt(Edicion)
-        parcel.writeString(Editorial)
-        parcel.writeString(ISBN)
-        parcel.writeString(Resumen)
-        parcel.writeArray(arrayOf(Tag))
-        parcel.writeByte(if (Favorito) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Book> {
-        override fun createFromParcel(parcel: Parcel): Book {
-            return Book(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Book?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+)
